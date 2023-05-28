94f8477224a7fb044782b05a81db0673fda8a6e5
funasaka-mediba <funasaka@mediba.jp>
2023-05-28 13:15:45 +0900

    画像フレームデザイン修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
                 color: "white",
                 padding: "4px 8px",
             }}>きょうのにゃんこ</button>
-            <div style={{ marginTop: 8, maxWidth: 500 }}>{loading || <img src={imageUrl} className={styles.img} />}</div>
+            <div className={styles.frame}>{loading || <img src={imageUrl} className={styles.img} />}</div>
         </div>
     );
 };
